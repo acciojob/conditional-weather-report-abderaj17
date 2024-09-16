01341bcfc0a82e4290049f4c5bb0445f2311a900
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2024-09-16 20:03:21 +0530

    add

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function WeatherDisplay(props) {
+    const {data} = props;
+
+    const tempStyle ={
+        color: data.temperature > 20 ? "red" : "blue"
+    };
+
+  return (
+    <div>
+        <p>Temperature: <span style={tempStyle}>{data.temperature}</span></p>
+        <p>conditions:{data.conditions}</p>
+    </div>
+  );
+}
+
+export default WeatherDisplay
