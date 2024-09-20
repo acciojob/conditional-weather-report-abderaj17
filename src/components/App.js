51e822e304d878ccb2179bc7016ff6b21229fb61
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2024-09-20 15:38:07 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
-
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 import WeatherDisplay from "./WeatherDisplay";
+
+
 const def = {temperature : 0, conditions : "Cool"}
 
-
 const App = () => {
-  const [data, setData] =useState(def)
 
-  useEffect(()=>{
+  const[data, setData] = useState(def)
+
+
+  useEffect(() => {
     setTimeout(() => {
-      setData({temperature:25, conditions:"Sunny"})
-    }, 1000);
+      setData({ temperature: 25, conditions: "Sunny" })
+    }, 1000)
   })
 
   return (
     <div>
-        <WeatherDisplay data={data} />
+      <WeatherDisplay data={data} />
     </div>
   )
 }
 
-export default App
+export default App;
